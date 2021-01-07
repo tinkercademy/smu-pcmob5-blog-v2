c30a56fdd3d70f26495ce6b690fcff55290ee8f6
YJ Soon <yjsoon@tinkertanker.com>
2021-01-07 22:12:42 +0800

    Set up loading screen for App.js

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { commonStyles } from "./styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  if (!loaded)
+    return (
+      <View style={commonStyles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
