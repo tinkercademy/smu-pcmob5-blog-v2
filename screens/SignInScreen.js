45cb57e682eb3d424555e09a9e66de08b485bc1b
YJ Soon <yjsoon@tinkertanker.com>
2021-01-10 18:21:39 +0800

    Improvement for SignIn - loading

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
+  ActivityIndicator,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
@@ -19,12 +20,14 @@ export default function SignInScreen({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorText, setErrorText] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function login() {
     console.log("---- Login time ----");
     Keyboard.dismiss();
 
     try {
+      setLoading(true);
       const response = await axios.post(API + API_LOGIN, {
         username,
         password,
@@ -32,8 +35,10 @@ export default function SignInScreen({ navigation }) {
       console.log("Success logging in!");
       // console.log(response);
       await AsyncStorage.setItem("token", response.data.access_token);
+      setLoading(false);
       navigation.navigate("Account");
     } catch (error) {
+      setLoading(false);
       console.log("Error logging in!");
       console.log(error.response);
       setErrorText(error.response.data.description);
@@ -62,10 +67,16 @@ export default function SignInScreen({ navigation }) {
           value={password}
           onChangeText={(input) => setPassword(input)}
         />
-        <TouchableOpacity onPress={login} style={styles.loginButton}>
-          <Text style={styles.buttonText}>Log in</Text>
-        </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={login} style={styles.loginButton}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </TouchableOpacity>
+          {loading ? (
+            <ActivityIndicator style={{ marginLeft: 20, marginBottom: 20 }} /> // adjust
+          ) : null}
+        </View>
         <Text style={styles.errorText}>{errorText}</Text>
+        <View style={{ height: 20, alignItems: "left" }}></View>
       </View>
     </TouchableWithoutFeedback>
   );
