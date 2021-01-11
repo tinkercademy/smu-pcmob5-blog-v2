e0013c32ab7e845d9e4d8c955f43fd6c17d71aa2
YJ Soon <yjsoon@tinkertanker.com>
2021-01-11 23:36:48 +0800

    Refactored auth out of sign in and up view

--- components/SignInSignUpView.js
+++ components/SignInSignUpView.js
@@ -10,61 +10,18 @@ import {
   Keyboard,
   ActivityIndicator,
 } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import axios from "axios";
-
-const API = "https://yjsoon.pythonanywhere.com";
-const API_LOGIN = "/auth";
-const API_SIGNUP = "/newuser";
+import { useAuth } from "../hooks/useAPI.js";
 
 export default function SignInSignUpView({ navigation, isSignIn }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorText, setErrorText] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  async function signup() {
-    console.log("---- Signing up ----");
-    Keyboard.dismiss();
-
-    try {
-      setLoading(true);
-      await axios.post(API + API_SIGNUP, {
-        username,
-        password,
-      });
-      console.log("Success signing up!");
-      login();
-    } catch (error) {
-      setLoading(false);
-      console.log("Error signing up!");
-      console.log(error.response);
-      setErrorText(error.response.data.description);
+  const [login, signup, loading, errorText] = useAuth(
+    username,
+    password,
+    () => {
+      navigation.navigate("Account"); // function to be run on successful login
     }
-  }
-
-  async function login() {
-    console.log("---- Login time ----");
-    Keyboard.dismiss();
-
-    try {
-      setLoading(true);
-      const response = await axios.post(API + API_LOGIN, {
-        username,
-        password,
-      });
-      console.log("Success logging in!");
-      // console.log(response);
-      await AsyncStorage.setItem("token", response.data.access_token);
-      setLoading(false);
-      navigation.navigate("Account");
-    } catch (error) {
-      setLoading(false);
-      console.log("Error logging in!");
-      console.log(error.response);
-      setErrorText(error.response.data.description);
-    }
-  }
+  );
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
--- hooks/useAPI.js
+++ hooks/useAPI.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 const API = "https://yjsoon.pythonanywhere.com";
 const API_WHOAMI = "/whoami";
+const API_LOGIN = "/auth";
+const API_SIGNUP = "/newuser";
 
 export function useUsername() {
   const [username, setUsername] = useState("");
@@ -37,4 +39,51 @@ export function useUsername() {
   }, [refresh]);
 
   return [username, loading, error, setRefresh];
+}
+
+export function useAuth(username, password, navigationCallback) {
+  const [loading, setLoading] = useState(false);
+  const [errorText, setErrorText] = useState("");
+
+  async function login() {
+    console.log("---- Signing in ----");
+
+    try {
+      setLoading(true);
+      const response = await axios.post(API + API_LOGIN, {
+        username,
+        password,
+      });
+      console.log("Success signing in!");
+      await AsyncStorage.setItem("token", response.data.access_token);
+      setLoading(false);
+      navigationCallback();
+    } catch (error) {
+      setLoading(false);
+      console.log("Error signing in!");
+      console.log(error);
+      setErrorText(error.response.data.description);
+    }
+  }
+
+  async function signup() {
+    console.log("---- Signing up ----");
+
+    try {
+      setLoading(true);
+      await axios.post(API + API_SIGNUP, {
+        username,
+        password,
+      });
+      console.log("Success signing up!");
+      login();
+    } catch (error) {
+      setLoading(false);
+      console.log("Error signing up!");
+      console.log(error);
+      setErrorText(error.response.data.description);
+    }
+  }
+
+  return [login, signup, loading, errorText];
 }
